8d4492801ce321386e8dd9c51ddf3c027383f4da
shubham <shubhamsharma10112003@gmail.com>
2024-03-12 23:56:03 +0530

    Basic Routes Built

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";          // Import the express package
+import dotenv from "dotenv";        // Import the dotenv package
+import authRoutes from "./routes/auth.routes.js";  // Import the authRoutes
+
+
+const app = express();  // Create an express app
+dotenv.config();        // Configure the dotenv package
+const PORT = process.env.PORT || 5000; // Define the port
+app.get("/",(req,res)=>{
+    res.send("Hello Worlds");
+});
+
+app.use("/api/auth",authRoutes); // Use the authRoutes
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));  // Start the server
