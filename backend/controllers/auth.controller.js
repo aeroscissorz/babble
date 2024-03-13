cd0f0da3c2226898340b7069baad406d827478f0
shubham <shubhamsharma10112003@gmail.com>
2024-03-13 16:55:52 +0530

    Signup endpoint created

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,15 +1,42 @@
+import User from "../models/user.model.js";
+
 export const signup = async(req, res) => {
     try{
-        const {fullName,username,password,confirmPassword,gender} = req.body;
+        const {fullName,userName,password,confirmPassword,gender} = req.body;
+
+        if(password !== confirmPassword){
+            return res.status(400).json({message:"Password does not match"});
+        };
+
+        const user = await User.findOne({userName});
+
+        if(user){
+            return res.status(400).json({message:"User already exists"});
+        };
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${userName}`;
+
+        const newUser = new User({
+            fullName,
+            userName,
+            password,
+            gender,
+            profilePic : gender === "male" ? boyProfilePic : girlProfilePic,
+        });
+
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            userName: newUser.userName,
+            profilePic: newUser.profilePic,
+        });
+
+    } catch (error) {
+        console.log("Error in signup",error.message);
+        res.status(500).json({ error:"Something went wrong"});
+
     }
-    catch(error){
-        console.log(error);}
 };
-
-export const login = (req, res) => {
-    console.log("Login User");
-}
-
-export const logout = (req, res) => {
-    console.log("Logout User");
-}
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
 import express from "express";          // Import the express package
 import dotenv from "dotenv";        // Import the dotenv package
+
 import authRoutes from "./routes/auth.routes.js";  // Import the authRoutes
 import { connect } from "mongoose";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 
 const app = express();  // Create an express app
-dotenv.config();        // Configure the dotenv package
 const PORT = process.env.PORT || 5000; // Define the port
-app.get("/",(req,res)=>{
-    res.send("Hello Worlds");
-});
+
+dotenv.config();        // Configure the dotenv package
+
+// app.get("/",(req,res)=>{
+//     res.send("Hello Worlds");
+// });
+app.use(express.json()); // Use the express.json() middleware
 
 app.use("/api/auth",authRoutes); // Use the authRoutes
 
