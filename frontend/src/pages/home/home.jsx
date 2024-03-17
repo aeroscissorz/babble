298ed32827324dcd299964a2c9082c5d8d8b88d7
shubham <shubhamsharma10112003@gmail.com>
2024-03-17 19:43:20 +0530

    Frontend is complete, all the things are hardcoded right now, but the frontend is complete. The backend is also complete, but the frontend is not connected to the backend yet. I will do that in the next commit.

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,10 +1,13 @@
+import MessageContainer from "../../components/messages/messageContainer"
 import Sidebar from "../../components/sidebar/sidebar"
 
 const Home = () => {
-  return <div className='sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding 
-  backdrop-filter backdrop-blur-lg bg-opacity-0'>
-    <Sidebar />
-  </div>
+  return (
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+      <Sidebar />
+      <MessageContainer />
+    </div>
+  )
 }
 
 export default Home
